9ab463be0e2ea4b77f6224e76a3a31e2a4649321
gajenderkumar85 <82595826+gajenderkumar85@users.noreply.github.com>
2024-08-28 22:40:44 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!....... (Modified by Gajender Kumar Chaudhary')
+        'body': json.dumps('Hello from Lambda!....... (Modified by Gajender Kumar Chaudhary on 28th august 2024')
     }
